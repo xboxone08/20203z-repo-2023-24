0011d0d74805164f82656476e553de3d372b4be7
Xbox One <sondad2008@gmail.com>
2023-05-07 19:18:13 -0400

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       choud                                                        #
+# 	Created:      5/7/2023, 7:08:30 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import Brain
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
