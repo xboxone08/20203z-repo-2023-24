32f51bb13026989d5bf19e160d5d5c21fbe1f3ce
Medhansh Choudhury <sondad2008@gmail.com>
2024-01-19 23:02:04 -0500

    NAHS 2.0 main

--- main.cpp
+++ main.cpp
@@ -3,23 +3,38 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// L2                   motor         1               
-// L1                   motor         11              
-// R3                   motor         20              
-// L3                   motor         19              
-// R1                   motor         18              
-// R2                   motor         17              
+// L1                   motor         19              
+// R1                   motor         10              
+// R2                   motor         8               
+// Poop                 inertial      9               
+// L2                   motor         20              
 // Controller1          controller                    
-// LiftSwitch           limit         A               
-// Potentiometer        potV2         C               
-// Intake               motor         16              
-// Lift                 motor         13              
-// CATAPULT             motor         10              
-// SWITCH               limit         B               
+// R3                   motor         3               
+// L3                   motor         15              
+// Intake               motor         6               
+// Shooter              motor         14              
+// Climb                motor         7               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 using namespace vex;
 competition Competition;
+
+typedef enum
+{
+    LIFT_STATE_AT_ZERO = 0,
+    LIFT_STATE_AT_GRAB,
+    LIFT_STATE_AT_READY,
+    LIFT_STATE_AT_LAUNCH,
+    LIFT_STATE_MOVING_UP,
+    LIFT_STATE_MOVING_DOWN,
+    LIFT_STATE_MOVING_TO_ZERO
+} lift_state_t;
+
+// #define LIFT_POSITION_ZERO        0.0  // Lift is resting on the switch
+// #define LIFT_POSITION_GRAB      100.0  // Lift is grabbing a ball
+// #define LIFT_POSITION_READY     240.0  // Lift is ready to grab a ball
+// #define LIFT_POSITION_LAUNCH    600.0  // Lift is ready for match loading
+
 
 /*---------------------------------------------------------------------------*/
 /*                             VEXcode Config                                */
@@ -44,7 +59,7 @@ Drive chassis(
 //Specify your drive setup below. There are seven options:
 //ZERO_TRACKER_NO_ODOM, ZERO_TRACKER_ODOM, TANK_ONE_ENCODER, TANK_ONE_ROTATION, TANK_TWO_ENCODER, TANK_TWO_ROTATION, HOLONOMIC_TWO_ENCODER, and HOLONOMIC_TWO_ROTATION
 //For example, if you are not using odometry, put ZERO_TRACKER_NO_ODOM below:
-ZERO_TRACKER_NO_ODOM,
+ZERO_TRACKER_ODOM,
 
 //Add the names of your Drive motors into the motor groups below, separated by commas, i.e. motor_group(Motor1,Motor2,Motor3).
 //You will input whatever motor names you chose when you configured your robot using the sidebar configurer, they don't have to be "Motor1" and "Motor2".
@@ -56,7 +71,7 @@ motor_group(L1, L2, L3),
 motor_group(R1, R2, R3),
 
 //Specify the PORT NUMBER of your inertial sensor, in PORT format (i.e. "PORT1", not simply "1"):
-PORT1,
+PORT9,
 
 //Input your wheel diameter. (4" omnis are actually closer to 4.125"):
 2.75,
@@ -64,11 +79,11 @@ PORT1,
 //External ratio, must be in decimal, in the format of input teeth/output teeth.
 //If your motor has an 84-tooth gear and your wheel has a 60-tooth gear, this value will be 1.4.
 //If the motor drives the wheel directly, this value is 1:
-0.60,
+1.0,
 
 //Gyro scale, this is what your gyro reads when you spin the robot 360 degrees.
 //For most cases 360 will do fine here, but this scale factor can be very helpful when precision is necessary.
-360,
+360.0,
 
 /*---------------------------------------------------------------------------*/
 /*                                  PAUSE!                                   */
@@ -98,7 +113,7 @@ PORT3,     -PORT4,
 //Input Forward Tracker center distance (a positive distance corresponds to a tracker on the right side of the robot, negative is left.)
 //For a zero tracker tank drive with odom, put the positive distance from the center of the robot to the right side of the drive.
 //This distance is in inches:
--2,
+7,
 
 //Input the Sideways Tracker Port, following the same steps as the Forward Tracker Port:
 1,
@@ -113,9 +128,15 @@ PORT3,     -PORT4,
 
 int current_auton_selection = 0;
 bool auto_started = false;
+int shoot = 0;
+bool released_ = true;
 
 void pre_auton(void) {
-  // Initializing Robot Configuration. DO NOT REMOVE!
+  chassis.set_heading(0);
+  Intake.setVelocity(100, percent);
+  Shooter.setVelocity(90, percent);
+  
+  // Initializing Robot Configuration. DO NOT REMOVE! 
   vexcodeInit();
   default_constants();
 
@@ -123,34 +144,16 @@ void pre_auton(void) {
     Brain.Screen.clearScreen();            //brain screen for auton selection.
     switch(current_auton_selection){       //Tap the brain screen to cycle through autons.
       case 0:
-        Brain.Screen.printAt(50, 50, "Drive Test");
+        Brain.Screen.printAt(50, 50, "Defensive Side 1");
         break;
       case 1:
-        Brain.Screen.printAt(50, 50, "Drive Test");
-        break;
-      case 2:
-        Brain.Screen.printAt(50, 50, "Turn Test");
-        break;
-      case 3:
-        Brain.Screen.printAt(50, 50, "Swing Test");
-        break;
-      case 4:
-        Brain.Screen.printAt(50, 50, "Full Test");
-        break;
-      case 5:
-        Brain.Screen.printAt(50, 50, "Odom Test");
-        break;
-      case 6:
-        Brain.Screen.printAt(50, 50, "Tank Odom Test");
-        break;
-      case 7:
-        Brain.Screen.printAt(50, 50, "Holonomic Odom Test");
+        Brain.Screen.printAt(50, 50, "Defensive Side 2");
         break;
     }
-    if(Brain.Screen.pressing()){
+    if(Brain.Screen.pressing()) {
       while(Brain.Screen.pressing()) {}
       current_auton_selection ++;
-    } else if (current_auton_selection == 8){
+    } else if (current_auton_selection == 2){
       current_auton_selection = 0;
     }
     task::sleep(10);
@@ -159,30 +162,12 @@ void pre_auton(void) {
 
 void autonomous(void) {
   auto_started = true;
-  switch(current_auton_selection){  
+  switch(current_auton_selection){
     case 0:
-      drive_test(); //This is the default auton, if you don't select from the brain.
+      defensive_side_1(); //This is the default auton, if you don't select from the brain.
       break;        //Change these to be your own auton functions in order to use the auton selector.
     case 1:         //Tap the screen to cycle through autons.
-      drive_test();
-      break;
-    case 2:
-      turn_test();
-      break;
-    case 3:
-      swing_test();
-      break;
-    case 4:
-      full_test();
-      break;
-    case 5:
-      odom_test();
-      break;
-    case 6:
-      tank_odom_test();
-      break;
-    case 7:
-      holonomic_odom_test();
+      offensive_side_1();
       break;
  }
 }
@@ -197,40 +182,13 @@ void autonomous(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-typedef enum {
-  INTAKE_STATE_PRIMED = 0,
-  INTAKE_STATE_ARM_LOWERING,
-  INTAKE_STATE_INTAKE,
-  INTAKE_STATE_HOLD,
-  INTAKE_STATE_ARM_RAISING,
-  INTAKE_STATE_INDEXING,
-  INTAKE_STATE_LOADED,
-  INTAKE_STATE_ARMING,
-  INTAKE_STATE_READY,
-  INTAKE_STATE_FIRE
-} intake_state_t;
 
-#define CATA_STATE_REST 0
-#define CATA_STATE_TRIGGERED 1
-#define CATA_STATE_RECOVER 2
 
 void usercontrol(void) {
-  // User control code here, inside the loop
-  intake_state_t intakeState = INTAKE_STATE_PRIMED;
-  int potentiometerBase = Potentiometer.value(percent);
 
-  Intake.setVelocity(100.0, percent);
-  Lift.setStopping(brake);
-  Lift.setMaxTorque(100, percent);
-  Intake.setStopping(brake);
-  CATAPULT.setStopping(hold);
-  CATAPULT.setVelocity(100, percent);
-  CATAPULT.setMaxTorque(100, percent);
+  
 
-  potentiometerBase = 40;
-  bool Cata_Button_Pressed;
-  bool pressed_before = false;
-  int Cata_State = CATA_STATE_RECOVER;
+  
 
   while (1) {
     // This is the main execution loop for the user control program.
@@ -246,192 +204,82 @@ void usercontrol(void) {
     //or chassis.control_holonomic(); for holo drive.
     chassis.control_arcade();
 
-    printf("%d\n", intakeState);
+    
     
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
 
-
-    if (pressed_before) {
-      if (!Controller1.ButtonL1.pressing())
-        pressed_before = false;
-    } else {
-      if (Controller1.ButtonL1.pressing() && intakeState != INTAKE_STATE_READY) {
-        intakeState = INTAKE_STATE_ARMING;
-        pressed_before = true;
-      }
-    }
-
-    // State machine for moving the tri-ball
-    // into and within the bot
-    switch (intakeState)
+    if (Controller1.ButtonR1.pressing())
     {
-    case INTAKE_STATE_PRIMED:
-      // Intake and lift motors off
-      // Starting state
-      Intake.stop();
-      Lift.stop();
-      if (Potentiometer.value(percent) >= potentiometerBase + 25)
-      {
-        intakeState = INTAKE_STATE_HOLD;
-      }
-      else if (Controller1.ButtonR1.pressing())
-      {
-        intakeState = INTAKE_STATE_ARM_LOWERING;
-      }
-      break;
-    case INTAKE_STATE_ARM_LOWERING:
-      // Intake and lift motors forward
-      // Lower intake mechanism
       Intake.spin(forward);
-      Lift.spin(forward);
-      // If lowering/intake button released before passing halfway point,
-      // bring arm back up, otherwise continue downward motion.
-      if (Potentiometer.value(percent) >= potentiometerBase + 25)
-      {
-        intakeState = INTAKE_STATE_INTAKE;
-      }
-      else if (!Controller1.ButtonR1.pressing() && Potentiometer.value(percent) < potentiometerBase + 13)
-      {
-        intakeState = INTAKE_STATE_ARM_RAISING;
-      }
-      break;
-    case INTAKE_STATE_INTAKE:
-      // Intake motor forward; lift motor off
-      // Actually do the intaking
-      Intake.spin(forward);
-      Lift.stop();
-      if (!Controller1.ButtonR1.pressing())
-      {
-        intakeState = INTAKE_STATE_HOLD;
-      }
-      break;
-    case INTAKE_STATE_HOLD:
-      // Intake and lift motors off
-      // Tri-ball in intake mechanism
-      Intake.stop();
-      Lift.stop();
-      if (LiftSwitch.pressing())
-      {
-        intakeState = INTAKE_STATE_PRIMED;
-      }
-      else if (Controller1.ButtonR1.pressing())
-      {
-        intakeState = INTAKE_STATE_ARM_RAISING;
-      }
-      else if (Controller1.ButtonR2.pressing())
-      {
-        intakeState = INTAKE_STATE_INDEXING;
-      }
-      else if (Controller1.ButtonL1.pressing())
-      {
-        intakeState = INTAKE_STATE_FIRE;
-      }
-      break;
-    case INTAKE_STATE_ARM_RAISING:
-      // Intake and lift motors reverse
+    }
+    else if (Controller1.ButtonR2.pressing())
+    {
       Intake.spin(reverse);
-      Lift.spin(reverse);
-      if (LiftSwitch.pressing())
-      {
-        potentiometerBase = Potentiometer.value(percent);
-        intakeState = INTAKE_STATE_PRIMED;
-      }
-      break;
-    case INTAKE_STATE_INDEXING:
-      // Intake motor forward, lift motor off
-      // Move tri-ball to shooting mechanism
+    }
+    else
+    {
       Intake.stop();
-      Lift.stop();
-      if (LiftSwitch.pressing())
-      {
-        // Do piston sequence
-        intakeState = INTAKE_STATE_LOADED;
-      }
-      break;
-    case INTAKE_STATE_LOADED: 
-      // Intake and lift motors off
-      // Ball in shooting mechanism
-      Intake.stop();
-      Lift.stop();
-      if  (Controller1.ButtonL1.pressing())
-      {
-        intakeState = INTAKE_STATE_ARMING;
-      }          
-      break;
-    case INTAKE_STATE_ARMING:
-      // Intake and lift motors forward
-      // Prepare to fire
-      if (!(Potentiometer.value(percent) >= potentiometerBase + 25)) {
-        Intake.spin(forward);
-        Lift.spin(forward);
-      } else {
-        intakeState = INTAKE_STATE_READY;
-      }
-      break;
-    case INTAKE_STATE_READY:
-      // Intake and lift motors off
-      // Ready to fire
-      Intake.stop();
-      Lift.stop();
-      Controller1.rumble("-");
-      if  (Controller1.ButtonL1.pressing())
-      {
-        intakeState = INTAKE_STATE_FIRE;
-      }
-      break;
-    case INTAKE_STATE_FIRE:
-      // Intake and lift motors off
-      // Actually firing
-      Intake.stop();
-      Lift.stop();
-      // Do catapult sequence
-      Cata_Button_Pressed = true;
-      intakeState = INTAKE_STATE_PRIMED;
-      break;
-    default:
-      break;
     }
 
-    // State machine for catapult
-    switch (Cata_State) {
-      case CATA_STATE_REST:
-        if (Cata_Button_Pressed)
-        {
-          CATAPULT.spin(forward);
-          Cata_State = CATA_STATE_TRIGGERED;
-        } 
-        break;
-      // Fire button was pressed
-      // (in prev. state machine)
-      case CATA_STATE_TRIGGERED:
-        if (Cata_Button_Pressed)
-        {
-          CATAPULT.spin(forward);
-        } 
-        if (!SWITCH)
-        {
-          Cata_State = CATA_STATE_RECOVER;
+    if (Controller1.ButtonL1.pressing())
+    {
+      // WingA.set(true);
+      // WingD.set(true);
+    }
+    else
+    {
+      // WingA.set(false);
+      // WingD.set(false);
+    }
+    if (Controller1.ButtonUp.pressing()) {
+      Climb.spin(forward);
+    }
+    else if (Controller1.ButtonDown.pressing()) {
+      Climb.spin(reverse);
+    }
+    else {
+      Climb.stop();
+    }
+
+    // if (Controller1.ButtonX.pressing()) {
+    //   // Only run once per press
+    //   if (released_) {
+    //     // Invert shoot
+    //     if (!shoot) {
+    //       shoot = 1;
+    //     } else {
+    //       shoot = 0;
+    //     }
+    //     released_ = false;
+    //   }
+    // } else {
+    //   released_ = true;
+    // }
+
+    if (Controller1.ButtonB.pressing()) {
+      if (released_) {
+        if (!shoot) {
+          shoot = -1;
+        } else {
+          shoot = 0;
         }
-        break;
-      // Get ready to fire again
-      // (bring catapult back down
-      // after firing)
-      case CATA_STATE_RECOVER:
-        if (Cata_Button_Pressed)
-        {
-          CATAPULT.spin(forward);
-        } 
-        if (SWITCH)
-        {
-          CATAPULT.stop();
-          Cata_Button_Pressed = false;
-          Cata_State = CATA_STATE_REST;
-        }
-        break; 
+        released_ = false;
+      }
+    } else {
+      released_ = true;
+    }
+
+    if (shoot == 1) {
+      Shooter.spin(forward);
+    } else if (shoot == -1) {
+      Shooter.spin(reverse);
+    } else {
+      Shooter.stop();
     }
   }
 }
+
 
 //
 // Main will set up the competition functions and callbacks.
